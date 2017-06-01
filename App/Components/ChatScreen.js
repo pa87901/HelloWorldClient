a3cd8e9164bfce0b21711274199c261d8459bacf
Preda Anuvatnujotikul <preda.anu@gmail>
2017-05-31 19:45:42 -0700

    Integrated basic chat feature.

--- App/Components/ChatScreen.js
+++ App/Components/ChatScreen.js
@@ -10,7 +10,8 @@ export default class Chat extends Component {
   constructor(props) {
     super(props);
     // Creating the socket-client instance will automatically connect to the server.
-    this.socket = SocketIOClient('http://localhost:3000');
+    // this.socket = SocketIOClient('http://ec2-35-167-135-24.us-west-2.compute.amazonaws.com:3000');
+    this.socket = SocketIOClient('http://localhost:3000')
     this.state = {
       messages: []
     };
@@ -26,21 +27,25 @@ export default class Chat extends Component {
     });
   }
 
+  onReceive(incMessage) {
+    this.socket.on('chat message', (msgs) => {
+      this.setState({
+        messages: msgs
+      });
+    });
+  }
 
   onSend(messages = []) {
     console.log('previous state', messages, this.state.messages);
     // Append the typed messsage to the previous state of messages.
-    this.setState((previousState) => {
-      let newStateMessages = previousState.messages.slice();
-      newStateMessages = newStateMessages.concat(messages);
-      console.log('previousState', previousState, 'newStateMessages', newStateMessages);
-      this.componentWillMount();
-      // return {
-      //   messages: GiftedChat.append(previousState.messsages, messages),
-      //   // messages: newStateMessages
-      // };
-    });
+    // this.setState((previousState) => {
+    //   let newStateMessages = previousState.messages.slice();
+    //   newStateMessages = newStateMessages.concat(messages);
+    //   console.log('previousState', previousState, 'newStateMessages', newStateMessages);
+      // this.componentWillMount();
+    // });
     // Emit typed message to server.
+    console.log('emitting', messages);
     this.socket.emit('chat message', messages);
   }
 
@@ -50,7 +55,9 @@ export default class Chat extends Component {
         messages={this.state.messages}
         onSend={this.onSend}
         user={{
-          _id:1,
+          _id: 1,
+          name: 'Alfred',
+          avatar: 'https://avatars0.githubusercontent.com/u/22867659?v=3&u=ce528a943e09cdde30d47ef590543c0ce41ff615&s=400'
         }}
       />
     );
--- App/Components/SearchNavigator.js
+++ App/Components/SearchNavigator.js
@@ -3,11 +3,13 @@ import { DrawerNavigator, StackNavigator, TabNavigator } from 'react-navigation'
 import SearchScreen from './SearchScreen';
 import ExploreNavigator from './ExploreNavigator';
 import LoginScreen from './LoginScreen';
+import ChatScreen from './ChatScreen';
 
 const SearchNavigator = StackNavigator({
   //Login: {screen: LoginScreen},
   Search: {screen: SearchScreen},
   Explore: {screen: ExploreNavigator},
+  Chat: {screen: ChatScreen}
 });
 
 export default SearchNavigator;
