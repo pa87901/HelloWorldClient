57ea0ae77bef8a7ea64118f88bc2f3fbb87031c6
Preda Anuvatnujotikul <preda.anu@gmail>
2017-05-31 19:53:00 -0700

    Integrated basic chat feature.

--- App/Components/InboxScreen.js
+++ App/Components/InboxScreen.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import ChatScreen from './ChatScreen';
 
 export default class InboxScreen extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
-      <View>
+      <ScrollView>
         <Text> INBOX </Text>
+<<<<<<< HEAD
       </View> 
     );
+=======
+        <Button
+          title="testChat"
+          onPress={() => this.props.navigation.navigate('Chat')}>
+        </Button>
+      </ScrollView> 
+    )
+>>>>>>> Integrated basic chat feature.
   }
 }
--- App/Components/SearchNavigator.js
+++ App/Components/SearchNavigator.js
@@ -4,12 +4,14 @@ import SearchScreen from './SearchScreen';
 import ExploreNavigator from './ExploreNavigator';
 import GuideProfileScreen from './GuideProfileScreen';
 import LoginScreen from './LoginScreen';
+import ChatScreen from './ChatScreen';
 
 const SearchNavigator = StackNavigator({
   //Login: {screen: LoginScreen},
   Search: {screen: SearchScreen},
   Explore: {screen: ExploreNavigator}
   // GuideProfile: {screen: GuideProfileScreen}
+  Chat: {screen: ChatScreen}
 });
 
 export default SearchNavigator;
