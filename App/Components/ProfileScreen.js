fdb82d473f3b1dca2944f422538552a3f63f29c9
Charles Kim <cjkim0119@gmail.com>
2017-05-31 20:19:43 -0700

    (component) in progress of modifying Profile Screen

--- App/Components/ProfileScreen.js
+++ App/Components/ProfileScreen.js
@@ -1,38 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Card, Divider, Icon, List, ListItem, Button } from 'react-native-elements';
+import { StyleSheet, Text, ScrollView } from 'react-native';
+import {
+  Card, Button, List, ListItem
+} from 'react-native-elements';
 
 export default class ProfileScreen extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      
-      usersInfo:
-        {
-          name: 'GuideName Here',
-        } 
-    } 
- 
   }
 
   render() {
     return (
-      <View>
-        <Card
-          title={this.state.usersInfo.name}
-          image={require('./JONSNOW.png')}
-        >
+      <ScrollView>
+        <ListItem
+          title="Guide Name"
+        />
+
+        <Tile
+         imageSrc={require('./JONSNOW.png')}
+         title="Guide Name"
+         caption="How are you feeling today?"
+        />
         <Button
             small
             raised
-            icon={{name:'directions-walk'}}
+            icon={{name: 'directions-walk'}}
             backgroundColor='#FF8C00'
             onPress={() => this.props.navigation.navigate('GuideQuestions')}
             title='Become A Guide!' 
           />
-        </Card>
-      </View> 
+      </ScrollView> 
     );
   }
 }
