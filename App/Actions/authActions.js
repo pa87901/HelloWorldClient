691da54a9963d2fc3b64ba32204debfe004e98d1
Charles Kim <cjkim0119@gmail.com>
2017-06-01 17:07:24 -0700

    (redux) refactor redux action format for Search and Auth

--- App/Actions/authActions.js
+++ App/Actions/authActions.js
@@ -1,17 +1,13 @@
 export function authenticate(authComplete, authData) {
-  //console.log(authComplete, authData)
-  return function(dispatch){
-    if(authComplete){
-      dispatch({
-        type: 'AUTHORIZATION_COMPLETED',
-        payload: authData
-      });
-    } else {
-      dispatch( {
-          type: 'AUTHORIZATION_FAILED',
-          payload: null
-        }
-      ) 
-    }
+  if (authComplete) {
+    return {
+      type: 'AUTHORIZATION_COMPLETED',
+      payload: authData
+    };
+  } else {
+    return {
+      type: 'AUTHORIZATION_FAILED',
+      payload: null
+    };
   }
 }
--- App/Actions/searchActions.js
+++ App/Actions/searchActions.js
@@ -1,36 +1,28 @@
 //placeholder for actions
 export function updateDate(date) {
-  return function(dispatch) {
-    dispatch({
-      type: 'UPDATE_DATE_FULFILLED',
-      payload: date
-    });
+  return {
+    type: 'UPDATE_DATE_FULFILLED',
+    payload: date
   };
 }
 
 export function updateCity(city) {
-  return function(dispatch) {
-    dispatch({
-      type: 'UPDATE_CITY_FULFILLED',
-      payload: city
-    });
+  return {
+    type: 'UPDATE_CITY_FULFILLED',
+    payload: city
   };
 }
 
 export function updateTravelers(number) {
-  return function(dispatch) {
-    dispatch({
-      type: 'UPDATE_TRAVELERS_FULFILLED',
-      payload: number
-    });
+  return {
+    type: 'UPDATE_TRAVELERS_FULFILLED',
+    payload: number
   };
 }
 
 export function updateSearchResult(result) {
-  return function(dispatch) {
-    dispatch({
-      type: 'UPDATE_SEARCH_RESULTS_FULFILLED',
-      payload: result
-    });
+  return {
+    type: 'UPDATE_SEARCH_RESULTS_FULFILLED',
+    payload: result
   };
 }
