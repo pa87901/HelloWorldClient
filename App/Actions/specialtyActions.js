16fcf4e2f59861c9dbe219d7d63e03ce7da72b90
Charles Kim <cjkim0119@gmail.com>
2017-06-03 10:35:20 -0700

    (guide specialties) create reducer/action, connect with specialty view

--- App/Actions/specialtyActions.js
+++ App/Actions/specialtyActions.js
@@ -1,6 +1,55 @@
 export function setSightseeing(boolean) {
   return {
     type: 'SET_SIGHTSEEING_FULFILLED',
-    payload: boolean
+    payload: boolean  
+  }
+}
+
+export function setMuseum(boolean) {
+  return {
+    type: 'SET_MUSEUM_FULFILLED',
+    payload: boolean  
+  }
+}
+
+export function setFood(boolean) {
+  return {
+    type: 'SET_FOOD_FULFILLED',
+    payload: boolean  
+  }
+}
+
+export function setNightlife(boolean) {
+  return {
+    type: 'SET_NIGHTLIFE_FULFILLED',
+    payload: boolean  
+  }
+}
+
+export function setSports(boolean) {
+  return {
+    type: 'SET_SPORTS_FULFILLED',
+    payload: boolean  
+  }
+}
+
+export function setMusic(boolean) {
+  return {
+    type: 'SET_MUSIC_FULFILLED',
+    payload: boolean  
+  }
+}
+
+export function setHistory(boolean) {
+  return {
+    type: 'SET_HISTORY_FULFILLED',
+    payload: boolean  
+  }
+}
+
+export function setPolitics(boolean) {
+  return {
+    type: 'SET_POLITICS_FULFILLED',
+    payload: boolean  
   }
 }
