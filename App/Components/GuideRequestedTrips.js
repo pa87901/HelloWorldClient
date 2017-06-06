5a76e1e3d8bbd5dc092a813a844f4a537d66bd60
Charles Kim <cjkim0119@gmail.com>
2017-06-05 19:56:21 -0700

    (feature) refactor GuideRequestedTrips view

--- App/Components/GuideRequestedTrips.js
+++ App/Components/GuideRequestedTrips.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { ScrollView, Text } from 'react-native';
-import { Card, Button, Divider } from 'react-native-elements';
+import { List, ListItem } from 'react-native-elements';
 
-export default class GuideRequestedOptions extends React.Component {
+class GuideRequestedTrips extends React.Component {
   constructor(props){
     super(props);
   }
@@ -10,85 +11,26 @@ export default class GuideRequestedOptions extends React.Component {
   render() {
     return (
       <ScrollView>
-        <Card title="Trip Requests">
-          <Card 
-            title="San Francisco on 08/31/17"
-          >
-            <Button
-              extraSmall
-              raised
-              title='Accept'
-              backgroundColor='#228b22'
-            >
-            </Button>
-            <Divider style={{width: 0 }}/> 
-            <Button
-              extraSmall
-              raised
-              title='Decline'
-              backgroundColor='#b22222'
-            >   
-            </Button> 
-          </Card>
-          <Card 
-            title="Berkeley on 07/23/17"
-          >
-            <Button
-              extraSmall
-              raised
-              title='Accept'
-              backgroundColor='#228b22'
-            >
-            </Button>
-            <Divider style={{width: 0 }}/> 
-            <Button
-              extraSmall
-              raised
-              title='Decline'
-              backgroundColor='#b22222'
-            >   
-            </Button> 
-          </Card>
-          <Card 
-            title="London on 06/13/17"
-          > 
-            <Button
-              extraSmall
-              raised
-              title='Accept'
-              backgroundColor='#228b22'
-            >
-            </Button>
-            <Divider style={{width: 0 }}/> 
-            <Button
-              extraSmall
-              raised
-              title='Decline'
-              backgroundColor='#b22222'
-            >   
-            </Button> 
-          </Card>
-          <Card 
-            title="Seoul on 12/25/17"
-          >
-            <Button
-              extraSmall
-              raised
-              title='Accept'
-              backgroundColor='#228b22'
-            >
-            </Button>
-            <Divider style={{width: 0 }}/> 
-            <Button
-              extraSmall
-              raised
-              title='Decline'
-              backgroundColor='#b22222'
-            >   
-            </Button> 
-          </Card>
-        </Card>
+        <List>
+          {this.props.booking.requestedGuideBookings.map((booking, key) => {
+              bookingDate = new Date(booking.date);
+
+              return (
+                <ListItem
+                  key={key}
+                  title={`${booking.user.full_name} (${booking.user.avg_rating})`}
+                  subtitle={
+                    `Requested Date: ${bookingDate.getMonth()}/${bookingDate.getDate()}/${bookingDate.getFullYear()}`}
+                />
+              )
+            }
+          )}
+        </List>
       </ScrollView>
     );
   }
 }
+
+const mapStateToProps = state => (state);
+
+export default connect(mapStateToProps)(GuideRequestedTrips);
