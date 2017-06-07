cdedf7e5aedc164e9264b7f202661d44c9b14763
Andrew Bedford-Hart <abedfordhart@gmail.com>
2017-06-07 16:31:55 -0700

    Implement marker based on current user location

--- App/Components/GuideTripsScreen.js
+++ App/Components/GuideTripsScreen.js
@@ -129,7 +129,7 @@ class GuideTripsScreen extends React.Component {
               <Text>
                 {booking.status}
               </Text>
-              <Button title='Details' onPress={()=>{this.props.navigation.navigate('MapScreen')}}/>   
+              <Button title='Map' onPress={()=>{this.props.navigation.navigate('MapScreen')}}/>   
               <Button title='Review' onPress={()=>{
                 this.setState({activeCard : i})
                 this.toggleReviewModal()
