e56b32c5880c2a43cffdd0e5250d99027a693806
Charles Kim <cjkim0119@gmail.com>
2017-06-08 15:36:56 -0700

    (checkout) combine card form view with booking policy view

--- App/Components/BookingPolicyScreen.js
+++ App/Components/BookingPolicyScreen.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Text, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
-import { Card, Button, Icon } from 'react-native-elements';
+import { PricingCard, Card, Button } from 'react-native-elements';
 import axios from '../axios.js';
+import stripe from 'tipsi-stripe';
+import testID from '../Utils/testID';
+import { STRIPE_PUBLISHABLE_KEY } from '../Config/config';
 
+stripe.init({
+  publishableKey: STRIPE_PUBLISHABLE_KEY
+});
 
 class BookingPolicyScreen extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loading: false,
+      token: null
+    };
+
     this.navigateToConfirmation = this.navigateToConfirmation.bind(this);
     this.bookTour = this.bookTour.bind(this);
   }
 
-  bookTour(){
-    let options = {
+  bookTour() {
+    const options = {
       travelerId: this.props.userProfile.profile.userId,
       guideFacebookId: this.props.profileSelection.selectedProfile.user.facebook_id,
       city: this.props.search.city,
@@ -23,12 +34,12 @@ class BookingPolicyScreen extends React.Component {
     };
     
     axios.post('api/bookings', options)
-      .then((res)=>{
-        console.log(res)
+      .then((res) => {
+        console.log(res);
       })
-      .catch((err)=>{
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   navigateToConfirmation() {
@@ -41,23 +52,49 @@ class BookingPolicyScreen extends React.Component {
   render() {
     console.log('PROPS', this.props);
 
+    const handleCardPayPress = async () => {
+      try {
+        this.setState({
+          loading: true,
+          token: null
+        });
+        const token = await stripe.paymentRequestWithCardForm({
+          smsAutofillDisabled: true,
+          requiredBillingAddressFields: 'full',
+          managedAccountCurrency: 'usd',
+        });
+
+        console.log('RESULT', token);
+        this.setState({
+          loading: false,
+          token,
+        });
+      } catch (error) {
+        console.log('ERROR', error);
+        this.setState({
+          loading: false,
+        });
+      }
+    };
+
+    const { loading, token } = this.state;
+
     return (
       <ScrollView>
+        <PricingCard
+          color='#FF8C00'
+          title='Your Total Cost'
+          price='$100'
+          info={['$10 Per Hour', '9AM - 9PM', 'Authorize Only', 'Payment Will Be Made When Trip Is Confirmed']}
+          button={{ title: 'Confirm and Request a Tour!', icon: 'check-circle' }}
+          onButtonPress={handleCardPayPress}
+        />
         <Card
           title='Terms & Conditions'
         >
-          <Text style={{marginBottom: 10}}>
+          <Text style={{ marginBottom: 10 }}>
             HelloWorld enforces terms to protect both tourist(s) and guide alike. Tourist(s) may cancel and review any penalties by viewing their travel plans and then clicking ‘Cancel’ on the appropriate reservation.
           </Text>
-          <Button
-            small
-            raised
-            icon={{name: 'check-circle'}}
-            backgroundColor='#FF8C00'
-            title='Confirm and Request a Tour!'
-            buttonStyle={{marginTop: 10}}
-            onPress={this.navigateToConfirmation}
-          />
         </Card>
       </ScrollView>
     );
@@ -67,3 +104,14 @@ class BookingPolicyScreen extends React.Component {
 const mapStateToProps = state => (state);
 
 export default connect(mapStateToProps)(BookingPolicyScreen);
+          
+
+          // <Button
+          //   small
+          //   raised
+          //   icon={{ name: 'check-circle' }}
+          //   backgroundColor='#FF8C00'
+          //   title='Confirm and Request a Tour!'
+          //   buttonStyle={{ marginTop: 10 }}
+          //   onPress={this.navigateToConfirmation}
+          // />
