ef94bb4bb2115642acbb574bc2fa85d08c4d63ad
Charles Kim <cjkim0119@gmail.com>
2017-06-07 21:54:54 -0700

    (feature) Add placeholder view for payment

--- ios/Pods/Target Support Files/Pods-HelloWorld/Pods-HelloWorld-resources.sh
+++ ios/Pods/Target Support Files/Pods-HelloWorld/Pods-HelloWorld-resources.sh
@@ -81,9 +81,11 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "$PODS_CONFIGURATION_BUILD_DIR/Lock/Auth0.bundle"
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/Stripe/Stripe.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "$PODS_CONFIGURATION_BUILD_DIR/Lock/Auth0.bundle"
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/Stripe/Stripe.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
