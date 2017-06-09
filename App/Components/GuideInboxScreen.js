5251fa64602f3cedb8767f95194adf1ddf65878f
Preda Anuvatnujotikul <preda.anu@gmail>
2017-06-08 17:10:43 -0700

    Able to navigate to tourist inbox now

--- App/Components/GuideInboxScreen.js
+++ App/Components/GuideInboxScreen.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import axios from '../axios';
 import { updateChats } from '../Actions/chatActions.js';
 import Conversation from './Conversation';
+import { updateUserGuideId } from '../Actions/userProfileActions.js';
+
 
 class GuideInboxScreen extends Component {
   constructor(props) {
@@ -15,9 +17,7 @@ class GuideInboxScreen extends Component {
   }
 
   componentWillMount() {
-    console.log('this.props in GuideInboxScreen', this.props, 'userGuideId', this.props.userProfile.userGuideId, 'chats', this.props.chat.chats);
-    // Need to change the global state this.props.chat.chats [] to be that belonging to this state's userGuideId.
-    // Make axios call for chats where guideId=...
+    // Get guideId from userId and set the redux state.
     axios.get(`/api/chats/byGuideId/${this.props.userProfile.userGuideId}`)
     .then(chats => {
       console.log('chats received by guideId', chats);
@@ -37,7 +37,7 @@ class GuideInboxScreen extends Component {
           chats: formattedMultipleChatMessage
         });
         this.props.dispatch(updateChats(formattedMultipleChatMessage));
-        this.componentDidMount();
+        // this.componentDidMount();
       }
     })
     .catch(error => {
@@ -46,30 +46,47 @@ class GuideInboxScreen extends Component {
   }
 
   componentDidMount() {
+    console.log('this.props in GuideInboxScreen', this.props, 'userGuideId', this.props.userProfile.userGuideId, 'chats', this.props.chat.chats);
+    // Need to change the global state this.props.chat.chats [] to be that belonging to this state's userGuideId.
+    // Make axios call for chats where guideId=...
+
+
+
+
+        let userNames = [];
+        this.state.chats
+          .map(chatObject => { console.log('chatObject from server in GuideInboxScreen', chatObject.user._id); return chatObject.user._id })
+          .filter((id, i, array) => { return array.indexOf(id) === i })
+        // With the userIds, make a call to the database to get customer/user names.
+          .forEach(user => {
+            axios.get(`api/users/byUserId/${user}`)
+            .then(userInfo => {
+              // console.log('userInfo from GET request in GuideInboxScreen', userInfo);
+              // if (typeof userInfo.data === 'object' && !Array.isArray(userInfo.data)) {
+              //   userInfo.data = [userInfo.data];
+              // }
+              userNames.push(userInfo.data);
+              console.log('userNames', userNames, this.state);
+              this.setState({
+                users: userNames
+              });
+            })
+            .catch(error => {
+              console.error('Error in getting userName from userId')
+            });
+          });
+
+
+
+
+
+
+
+
+
     // console.log('this.state in GuideInboxScreen', this.state);
     // Iterate through chat [] and reduce for unique userIds.
-    let userNames = [];
-    this.state.chats
-      .map(chatObject => { console.log('chatObject from server in GuideInboxScreen', chatObject.user._id); return chatObject.user._id })
-      .filter((id, i, array) => { return array.indexOf(id) === i })
-    // With the userIds, make a call to the database to get customer/user names.
-      .forEach(user => {
-        axios.get(`api/users/byUserId/${user}`)
-        .then(userInfo => {
-          // console.log('userInfo from GET request in GuideInboxScreen', userInfo);
-          // if (typeof userInfo.data === 'object' && !Array.isArray(userInfo.data)) {
-          //   userInfo.data = [userInfo.data];
-          // }
-          userNames.push(userInfo.data);
-          console.log('userNames', userNames, this.state);
-          this.setState({
-            users: userNames
-          });
-        })
-        .catch(error => {
-          console.error('Error in getting userName from userId')
-        });
-      });
+    
   }
 
   render() {
